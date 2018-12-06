b887afe986a46ea6e4e168e5fe56f6dfe1fc2859
Raghu Kaippully <rkaippully@gmail.com>
2018-12-06 18:31:36 +0530

    Simplified day 3

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,59 +1,64 @@
 module Day3 where
 
-import           Data.List            (find, group, sort)
+import           Data.List            (groupBy, sortOn)
 import           Data.Set             (Set)
 import qualified Data.Set             as Set
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 
+
+newtype ClaimId = ClaimId Int
+  deriving (Eq, Ord, Show)
+newtype X = X Int
+  deriving (Eq, Ord, Show)
+newtype Y = Y Int
+  deriving (Eq, Ord, Show)
+
 data Claim = Claim {
-  claimId  :: String
-  , left   :: Int
-  , top    :: Int
-  , width  :: Int
-  , height :: Int
-  } deriving Show
+  claimId  :: ClaimId
+  , xCoord :: X
+  , yCoord :: Y
+  } deriving (Show)
+
+coord :: Claim -> (X, Y)
+coord (Claim _ x y) = (x, y)
+
+equalCoord :: Claim -> Claim -> Bool
+equalCoord x y = coord x == coord y
+
+-- Claims grouped together by their coordinates
+groupedClaims :: String -> [[Claim]]
+groupedClaims = groupBy equalCoord . sortOn coord . getClaims
+
+day3part1 :: String -> Int
+day3part1 = length . filter (\x -> length x > 1) . groupedClaims
+
+day3part2 :: String -> Int
+day3part2 s =
+  let
+    pickClaimIds :: ([Claim] -> Bool) -> [[Claim]] -> Set ClaimId
+    pickClaimIds f = Set.fromList . concatMap (map claimId) . filter f
+
+    goodClaimIds = pickClaimIds (\x -> length x == 1) $ groupedClaims s
+    badClaimIds = pickClaimIds (\x -> length x > 1) $ groupedClaims s
+  in
+    case Set.findMin $ Set.difference goodClaimIds badClaimIds of
+      ClaimId n -> n
+
+getClaims :: String -> [Claim]
+getClaims s =
+  case parse (many parseClaim) "" s of
+    Left e   -> error $ show e
+    Right xs -> concat xs
 
 type Parser = Parsec () String
 
-claim :: Parser Claim
-claim = Claim
-        <$> (char '#' *> digitChar `manyTill` char ' ' <* string "@ ")
-        <*> (read <$> digitChar `manyTill` char ',')
-        <*> (read <$> digitChar `manyTill` char ':' <* char ' ')
-        <*> (read <$> digitChar `manyTill` char 'x')
-        <*> (read <$> digitChar `manyTill` newline)
-
-day3part1 :: String -> String
-day3part1 s =
-  case parse (many claim) "" s of
-    Left e       -> show e
-    Right claims -> show $ length $ filter (\x -> length x > 1) $ group $ sort $ concatMap toCoords claims
-
-toCoords :: Claim -> [(Int, Int)]
-toCoords c = do
-  x <- [left c..(left c + width c - 1)]
-  y <- [top c..(top c + height c - 1)]
-  return (x, y)
-
-data Coord = Coord {
-  cClaimId  :: String
-  , cCoords :: [(Int, Int)]
-  } deriving Show
-
-day3part2 :: String -> String
-day3part2 s =
-  case parse (many claim) "" s of
-    Left e       -> show e
-    Right claims -> show $ f claims
-  where
-    f :: [Claim] -> Maybe Coord
-    f claims =
-      let
-        cs = map (\c -> Coord (claimId c) (toCoords c)) claims
-        coords = Set.fromList $ concat $ filter (\x -> length x == 1) $ group $ sort $ concatMap cCoords cs
-      in
-        find (isGoodClaim coords) cs
-
-    isGoodClaim :: Set (Int, Int) -> Coord -> Bool
-    isGoodClaim gs c = all (`Set.member` gs) (cCoords c)
+parseClaim :: Parser [Claim]
+parseClaim = do
+  cId <- read <$> (char '#' >> many digitChar)
+  left <- read <$> (string " @ " >> many digitChar)
+  top <- read <$> (char ',' >> many digitChar)
+  width <- read <$> (string ": " >> many digitChar)
+  height <- read <$> (char 'x' >> many digitChar)
+  _ <- newline
+  return [Claim (ClaimId cId) (X x) (Y y) | x <- [left..(left+width-1)], y <- [top..(top+height-1)]]
